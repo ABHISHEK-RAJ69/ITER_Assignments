c11766a4978351776475d7766b8cbbed4c27d805
Abhishek Raj <abhishekraj605912@gmail.com>
2024-11-25 01:23:13 +0530

    Added Q18

---
+++ 1st Semester/Output Questions/Conditional Statement/Q18.java
@@ -0,0 +1,12 @@
+public class Q18 {
+    public static void main(String[] args) {
+        int a = 10 ; 
+        int b = 9;
+        if (a> b) {
+            System.out.println(" a is greater ");
+            System.out.println(" I am not in if block ");
+        }
+    }
+}
+//  OUTPUT :-  a is greater 
+//  I am not in if block 
