4b183d1d06fb782d973d84055f963552eaa6aab3
Abhishek Raj <abhishekraj605912@gmail.com>
2024-11-24 20:58:34 +0530

    Added Q10

---
+++ 1st Semester/Output Questions/Conditional Statement/Q10.java
@@ -0,0 +1,21 @@
+public class Q10 {
+    public static void main(String[] args) {
+        int k =65;
+        switch (k) {
+            default:
+                System.out.println("Website");
+            case 6:
+                System.out.println("Merit");
+            case 'k':
+                System.out.println("campus");
+            case 'j':
+                System.out.println("Java");
+                break;
+        }
+    }
+}
+
+// OUTPUT :-
+// Merit
+// campus
+// Java
