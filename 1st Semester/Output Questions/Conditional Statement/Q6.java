7ceb95c2e7d86d020e1d2cad1df8890c1796c214
Abhishek Raj <abhishekraj605912@gmail.com>
2024-11-24 20:07:00 +0530

    Added Q6

---
+++ 1st Semester/Output Questions/Conditional Statement/Q6.java
@@ -0,0 +1,11 @@
+public class Q6 {
+    public static void main(String[] args) {
+        if (true) {
+            System.out.println("hello");
+            // break; 
+            //break cannot be used outside of a loop or a switch
+        }
+    }
+}
+// ERROR
+//hello if error is removed
