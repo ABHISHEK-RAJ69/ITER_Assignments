20233ce8cdfaacc569f5d4385febc8a33cc0630c
Abhishek Raj <abhishekraj605912@gmail.com>
2024-11-25 00:14:59 +0530

    Added Q12

---
+++ 1st Semester/Output Questions/Conditional Statement/Q12.java
@@ -0,0 +1,12 @@
+public class Q12 {
+    public static void main(String[] args) {
+        int ok =10;
+        switch (ok) {
+            case 10:  
+            case 10:
+            case 10: System.out.println("True");
+        }
+    }
+}
+
+// Error :- Duplicate case
