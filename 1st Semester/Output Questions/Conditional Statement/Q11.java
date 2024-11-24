fb6c0fb8f5cb65a44e01a3386f33342ecc9c21be
Abhishek Raj <abhishekraj605912@gmail.com>
2024-11-24 22:43:16 +0530

    Added Q11

---
+++ 1st Semester/Output Questions/Conditional Statement/Q11.java
@@ -0,0 +1,12 @@
+public class Q11 {
+    public static void main(String[] args) {
+        boolean x = true ;
+        boolean y = false;
+        if ((x && y )| (x || y )){
+            //( 0 and 1= 0) or ( 0 or 1 =1) => 0 or 1 = 1 => ture 
+            System.out.println(true);
+        }else{
+            System.out.println(false);
+        }
+    }
+}
