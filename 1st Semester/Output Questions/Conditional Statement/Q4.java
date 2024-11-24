4072d4a52b067167cd5dd994f97006c9ba957022
Abhishek Raj <abhishekraj605912@gmail.com>
2024-11-24 19:45:42 +0530

    Added Q4

---
+++ 1st Semester/Output Questions/Conditional Statement/Q4.java
@@ -0,0 +1,14 @@
+public class Q4 {
+    public static void main(String[] args) {
+        int n = 5;
+        if (n<=4) {
+            System.out.println("n is less than 4");
+            System.out.println("cheking");
+        }else{
+            System.out.println("n is grater than equal to 4");
+        }
+    }
+    
+}
+// OUTPUT:-
+// n is grater than equal to 4
