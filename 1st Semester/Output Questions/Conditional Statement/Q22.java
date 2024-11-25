3bbe6e451e1822a6c796ee16a2bfb285774f2e48
Abhishek Raj <abhishekraj605912@gmail.com>
2024-11-25 12:31:33 +0530

    Added Q22

---
+++ 1st Semester/Output Questions/Conditional Statement/Q22.java
@@ -0,0 +1,12 @@
+public class Q22 {
+
+    public static void main(String[] args) {
+        int x = 1; 
+        if ((boolean)x ==true){
+            System.out.println("True");
+        }else{
+            System.out.println("False");
+        }
+    }
+}
+//  ERROR :- Cannot cast from int to boolean
