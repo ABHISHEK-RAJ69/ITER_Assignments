8de571d0aef675c7214a036177f5ff784b911b8a
Abhishek Raj <abhishekraj605912@gmail.com>
2024-11-25 12:33:03 +0530

    Added Q21

---
+++ 1st Semester/Output Questions/Conditional Statement/Q21.java
@@ -0,0 +1,12 @@
+public class Q21 {
+    public static void main(String[] args) {
+        if (true && false && true || false) {
+            System.out.println("True");
+        }else{
+            System.out.println("False");
+        }
+    }
+}
+// Alsways false statement true && false
+// OUTPUT :- 
+// False 
