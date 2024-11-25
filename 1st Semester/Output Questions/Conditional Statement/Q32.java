e7a3938c3d78275d3ee59c19a3d4761ae608d885
Abhishek Raj <abhishekraj605912@gmail.com>
2024-11-26 02:18:32 +0530

    Added Q32

---
+++ 1st Semester/Output Questions/Conditional Statement/Q32.java
@@ -0,0 +1,13 @@
+public class Q32 {
+    public static void main(String[] args) {
+        int var1 = 5 ;
+        int var2 = 6 ;
+        if ((var2=1) == var1){
+            System.out.println(var2);
+        }else{
+            System.out.println(++var2);
+        }
+    }
+}
+// Output:-
+// 2
