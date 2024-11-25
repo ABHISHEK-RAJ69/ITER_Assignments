9833d7404b5d8e4f15ae2c68d9fcded38d690110
Abhishek Raj <abhishekraj605912@gmail.com>
2024-11-26 01:52:28 +0530

    Added Q27

---
+++ 1st Semester/Output Questions/Conditional Statement/Q27.java
@@ -0,0 +1,17 @@
+public class Q27 {
+    public static void main(String[] args) {
+        int marks = 80;
+        if (marks > 70 ){ //true
+            System.out.println("Distinction");
+        }
+        if (marks>35) { //ture
+            System.out.println("Pass");
+        }else{
+            System.out.println("Fail");
+            System.out.println("Better luck next time");
+        }
+    }
+}
+// OUTPUT:-
+// Distinction
+// Pass
