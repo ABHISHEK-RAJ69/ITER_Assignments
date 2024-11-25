3e35baac32e6629c336f38ed184c4c4b2fea9354
Abhishek Raj <abhishekraj605912@gmail.com>
2024-11-26 01:54:41 +0530

    Added Q28

---
+++ 1st Semester/Output Questions/Conditional Statement/Q28.java
@@ -0,0 +1,13 @@
+public class Q28 {
+    public static void main(String[] args) {
+        int a = 15;
+        int b =25;
+        if ((a<b )|| (a =5)>15){
+            System.out.println(a);
+        }else{
+            System.out.println(b);
+        }
+    }
+}
+// OUTPUT:-
+// 15
