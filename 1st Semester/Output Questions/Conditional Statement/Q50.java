3fe2a8e3eb37f70bd28fa493dd480007b9d2af16
Abhishek Raj <abhishekraj605912@gmail.com>
2024-11-27 00:57:48 +0530

    Added Q50

---
+++ 1st Semester/Output Questions/Conditional Statement/Q50.java
@@ -0,0 +1,17 @@
+public class Q50 {
+    public static void main(String[] args) {
+        if(int q =0){  
+            System.out.println("if part");
+        }
+        else{
+            System.out.println("else part");
+        }
+        return 0;
+    }
+}
+// Error
+//Syntax error on token "int", delete this token
+
+// q cannot be resolved to a variable
+
+// Void methods cannot return a value
