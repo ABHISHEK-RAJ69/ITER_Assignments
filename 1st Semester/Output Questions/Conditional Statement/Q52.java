074097188740ea0932d9d2e5b28acbf85c7e5f86
Abhishek Raj <abhishekraj605912@gmail.com>
2024-11-27 02:09:01 +0530

    Added Q52

---
+++ 1st Semester/Output Questions/Conditional Statement/Q52.java
@@ -0,0 +1,10 @@
+public class Q52 {
+    public static void main(String[] args) {
+        int a = 10 , b = 5;
+        if( a>>1==b){
+            System.out.println("ABC");
+        }
+    }
+}
+// output:-
+// ABC 
