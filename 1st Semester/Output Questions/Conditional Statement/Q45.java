c26ba9099e09745a2cc0724e3c5c57d062f515a0
Abhishek Raj <abhishekraj605912@gmail.com>
2024-11-27 00:36:59 +0530

    Added Q45

---
+++ 1st Semester/Output Questions/Conditional Statement/Q45.java
@@ -0,0 +1,12 @@
+public class Q45 {
+    public static void main(String[] args) {
+        float a = 0.7f;
+        if(a < 0.7){ // for 0.7 double > float
+            System.out.println("Hello World");
+        }else{
+            System.out.println("Hello ITER");
+        }
+    }
+}
+// output:-
+// Hello World
