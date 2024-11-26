9a27214a60b05e0c6cd192c469b485414310697a
Abhishek Raj <abhishekraj605912@gmail.com>
2024-11-27 00:48:51 +0530

    Added Q48

---
+++ 1st Semester/Output Questions/Conditional Statement/Q48.java
@@ -0,0 +1,21 @@
+public class Q48 {
+    public static void main(String[] args) {
+        switch (* (3+ "I LOVE""ABCD"+3)){
+            case 'A': //string comparing with char
+                System.out.println("Apple Mac"); 
+                break; 
+            case 'B': 
+                System.out.println("Windows"); 
+                break; 
+            case 'C': 
+                System.out.println("Great Linux"); 
+                break; 
+            default: 
+                System.out.println("All the above");
+        }
+    }
+}
+// Error 
+// Full of error 
+// Syntax error on token "*", delete this token
+// Syntax error on token ""ABCD"", delete this token
