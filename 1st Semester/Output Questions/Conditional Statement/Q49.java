1397fa5e152adb4dd46bb927c8a4ed9b1fc83e39
Abhishek Raj <abhishekraj605912@gmail.com>
2024-11-27 00:53:17 +0530

    Added Q49

---
+++ 1st Semester/Output Questions/Conditional Statement/Q49.java
@@ -0,0 +1,14 @@
+public class Q49 {
+    public static void main(String[] args) {
+        if(!(System.out.println("x"))){
+            System.out.println("if part ");
+        }
+        else{
+            System.out.println("else part ");
+        }
+    }
+}
+// Error 
+    //The operator ! is undefined for the argument type(s) void
+
+    // Type mismatch: cannot convert from void to boolean
