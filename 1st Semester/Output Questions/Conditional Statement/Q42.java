0b9025d65a25625976de11d9b908d050d3bdc91c
Abhishek Raj <abhishekraj605912@gmail.com>
2024-11-27 00:12:40 +0530

    Added Q42

--- 1st Semester/Output Questions/Conditional Statement/Q42.java
+++ 1st Semester/Output Questions/Conditional Statement/Q42.java
@@ -1,4 +1,17 @@
-package PACKAGE_NAME;
-
 public class Q42 {
+    public static void main(String[] args) {
+        int a = 12 + 21 * 3 - 9/2; // 71 
+        int b = 14 - 32 * 4 + 175 / 8 -3; // -96
+        // System.out.println( a +" " +b);
+        if (++a > 71 && --b < 20){
+            System.out.println("a = "+a + " b = "+ b );
+        }
+        if ( b-- == -97 || a-- < 100){
+            System.out.println("a = "+ a + " b = "+b);
+        }
+    }
 }
+
+// output:-
+// a = 72 b = -97
+// a = 72 b = -98
