0834b296761ae7f81727c52b627d6f070abf7d26
Abhishek Raj <abhishekraj605912@gmail.com>
2024-11-27 19:53:10 +0530

    fixed Q54

--- 1st Semester/Output Questions/Conditional Statement/Q54.java
+++ 1st Semester/Output Questions/Conditional Statement/Q54.java
@@ -1,6 +1,8 @@
 public class Q54 {
+    int x =4;
     public static void main(String[] args) {
         int a =10 , b =5;
+        
         switch (a <b ? a: b-1){ //4
             case 5:
                 System.out.println("WOW");
@@ -8,7 +10,7 @@ public class Q54 {
             case 3 :
                 System.out.println("Its working ");
                 break;
-            case  4: // x=>4
+            case  x:
                 System.out.println("Ooh...");
                 break;
             default:
@@ -17,4 +19,4 @@ public class Q54 {
     }
 }
 // output:-
-// Ooh... 
+// Cannot make a static reference to the non-static field x
