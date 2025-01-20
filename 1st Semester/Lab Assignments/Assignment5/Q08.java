19e668b4f672c9cf007f202463a9c41464147042
Abhishek Raj <abhishekraj605912@gmail.com>
2025-01-20 20:01:29 +0530

    Optimized Assignment5-Q08

--- 1st Semester/Lab Assignments/Assignment5/Q08.java
+++ 1st Semester/Lab Assignments/Assignment5/Q08.java
@@ -8,11 +8,11 @@ public class Q08 {
         Scanner sc = new Scanner(System.in);
 		System.out.println("Enter a number : ");
 		int n = sc.nextInt();
-		int sum=1;
-		for (int i=2;i<=n;i++) {
-			for(int j =1;j<=i;j++) {
-				sum+=j;
-			}
+		int sum=0 ;
+		int loopsum = 0 ;
+		for (int i=1;i<=n;i++) {
+			loopsum +=i;
+			sum += loopsum ;
 		}
 		System.out.println("Sum of the series is : "+sum);
 		
