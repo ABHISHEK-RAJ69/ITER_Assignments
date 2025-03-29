3e4f270039e7f6a5c67672fb4aae6c17ab078eba
Abhishek Raj <abhishekraj605912@gmail.com>
2025-03-30 01:09:41 +0530

    Added Q10

--- 2nd Semester/Lab Assignments/Assignment2/Q10$1/Student.java
+++ 2nd Semester/Lab Assignments/Assignment2/Q10$1/Student.java
@@ -4,11 +4,11 @@ import Assignment2.Q10$2.*;
 public class Student implements Sports {
     String name ;
     int roll ;
-    void inputDetails(String name , int roll){
+    public void inputDetails(String name , int roll){
         this.name = name ;
         this.roll = roll ;
     }
-    void showDetails(){
+    public void showDetails(){
         System.out.println("Name = "+this.name );
         System.out.println("roll = "+ this.roll);
     }
