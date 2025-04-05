8860c3c53c5adec96ee4b136a70d7778a32cdaf9
Abhishek Raj <abhishekraj605912@gmail.com>
2025-04-05 19:17:51 +0530

    Addeed Q07

---
+++ 2nd Semester/Lab Assignments/Assignment3/Q07.java
@@ -0,0 +1,17 @@
+package Assignment3;
+
+public class Q07 {
+
+	public static void main(String[] args) {
+		System.out.println(fact(5));
+
+	}
+	static int fact(int a ) {
+		if(a ==1 || a==0) {
+			return a;
+		
+		}
+		return a*fact(a-1);
+	}
+
+}
