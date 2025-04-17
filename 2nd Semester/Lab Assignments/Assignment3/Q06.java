901927ab1322cde3f3ea1994c8b5644c37c3fb58
Abhishek Raj <abhishekraj605912@gmail.com>
2025-04-17 16:18:07 +0530

    Added output

--- 2nd Semester/Lab Assignments/Assignment3/Q06.java
+++ 2nd Semester/Lab Assignments/Assignment3/Q06.java
@@ -5,6 +5,8 @@ public class Q06 {
 	public static void main(String[] args) {
 		Integer[] a = new Integer[]{1,2,3,4,1,2,1,1};
 		System.out.println(count(a,1));
+		Character[] b = new Character[]{'3','3','e','r'};
+		System.out.println(count(b, '3'));
 
 	}
 	public static <T> int count(T[] array, T item) {
@@ -18,3 +20,8 @@ public class Q06 {
 	}
 
 }
+/*
+Output:-
+4
+2
+ */
