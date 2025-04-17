a9aecb3f157eb01dad679c307a4b591715136064
Abhishek Raj <abhishekraj605912@gmail.com>
2025-04-17 15:14:20 +0530

    Fixed some bugs

--- 2nd Semester/Lab Assignments/Assignment3/Q01.java
+++ 2nd Semester/Lab Assignments/Assignment3/Q01.java
@@ -5,22 +5,21 @@ import java.util.Scanner;
 public class Q01 {
 
 	public static void main(String[] args) {
-		
-		Scanner sc = new Scanner(System.in);
+
+		Scanner sc = new Scanner(System.in);		
 		
 		System.out.println("ENter a number ");
 		int n = sc.nextInt();
-		if(n<0) {
-			try {
+		try{
+			if(n<0) {
 				throw new NumberFormatException("Number is negative");
-			}catch(NumberFormatException e) {
-				System.out.println(e.getMessage());
-			}finally {
-				sc.close();
 			}
-		
+			System.out.println("No error Found");
+		}catch(NumberFormatException e) {
+			System.out.println(e.getMessage());
+		}finally {
+			sc.close();
 		}
-		System.out.println("No error Found");
 	}
 
 }
