09c887239c8d1bc23608fcab679c043da3b1293a
Abhishek Raj <abhishekraj605912@gmail.com>
2025-04-17 16:24:41 +0530

    fixed some bugs

--- 2nd Semester/Lab Assignments/Assignment3/Q07.java
+++ 2nd Semester/Lab Assignments/Assignment3/Q07.java
@@ -1,14 +1,13 @@
 package Assignment3;
 
 public class Q07 {
-
 	public static void main(String[] args) {
 		System.out.println(fact(5));
 
 	}
 	static int fact(int a ) {
 		if(a ==1 || a==0) {
-			return a;
+			return 1;
 		
 		}
 		return a*fact(a-1);
